94b8d68db93a3322a5425788b6e704f14d4c7b47
Aaditya Patil <123950344+Aaditya123-exe@users.noreply.github.com>
2023-10-24 07:05:01 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    bool valid(string prefix,int i,int j)
+    {
+        
+        while(i<j)
+        {
+            if(prefix[i]!=prefix[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int palindromicPartition(string str)
+    {
+        int n=str.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            int minCost=INT_MAX;
+            for(int j=i;j<n;j++)
+            {
+                if(valid(str,i,j))
+                {
+                    int cost=1+dp[j+1];
+                    minCost=min(minCost,cost);
+                }
+            }
+            dp[i]=minCost;
+        }
+        return dp[0]-1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
